b8e8b2a6af8dca084c78b139bc7abc5e7bacf7db
yw662 <yw662@njit.edu>
2018-06-04 21:14:35 -0400

    first version

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # mime-magic
 
 Use command `file -bE --mime-type` to detect mime type.
+
 If file gives out `text/plain`,
 then use `mime.getType`.
 
+```js
+const mimeMagic = require('mime-magic');
+
+mimeMagic(path, type => {
+    ...
+});
+````
---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mime = require('mime');
+const {exec} = require('child_process');
+
+module.exports = (path, cb) => {
+  exec('file -bE --mime-type ' + path, (e, o) => {
+    o = o.trim();
+    if (e || o == 'text/plain') {
+      setImmediate(cb, mime.getType(path));
+    } else {
+      setImmediate(cb, o);
+    }
+  });
+};
